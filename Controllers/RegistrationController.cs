65a70f579f8917b64e321bf8a499b3917ea2fd97
Rajeev Ranjan Singh <rajeevranjansingh8272@gmail.com>
2025-09-11 22:43:28 +0530

    Add project files.

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegistrationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        public RegistrationController() { }
+        [HttpPost("Register")]
+        public ActionResult<string> ActionResult(string username, string password)
+        {
+            return "Hello " + username;
+        }
+    }
+}
